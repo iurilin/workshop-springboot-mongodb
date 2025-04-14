efcdf036395225f03b825ee20de13cbfe022999f
Iuri Lin <audazzk@gmail.com>
2025-04-14 10:07:07 -0300

    Database Instancy

---
+++ src/main/java/com/iuri/workshopmongo/config/Instantiation.java
@@ -0,0 +1,31 @@
+package com.iuri.workshopmongo.config;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.iuri.workshopmongo.domain.User;
+import com.iuri.workshopmongo.repository.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner{
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		userRepository.deleteAll();
+		
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+	}
+
+}
