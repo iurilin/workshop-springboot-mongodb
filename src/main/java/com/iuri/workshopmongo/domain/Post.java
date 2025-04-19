1692b5039766b5f22579a4ca604b537f26c542a9
Iuri Lin <audazzk@gmail.com>
2025-04-19 12:57:50 -0300

    Adding comments to posts

--- src/main/java/com/iuri/workshopmongo/domain/Post.java
+++ src/main/java/com/iuri/workshopmongo/domain/Post.java
@@ -1,11 +1,14 @@
 package com.iuri.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
 
 import com.iuri.workshopmongo.dto.AuthorDTO;
+import com.iuri.workshopmongo.dto.CommentDTO;
 
 public class Post implements Serializable {
 	private static final long serialVersionUID = 1L;
@@ -16,6 +19,8 @@ public class Post implements Serializable {
 	private String title;
 	private String body;
 	private AuthorDTO author;
+	
+	private List<CommentDTO> comments = new ArrayList<>();
 
 	public Post() {
 	}
@@ -68,6 +73,15 @@ public class Post implements Serializable {
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
 	}
+	
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}
+
 
 	@Override
 	public int hashCode() {
@@ -93,5 +107,4 @@ public class Post implements Serializable {
 			return false;
 		return true;
 	}
-
 }
