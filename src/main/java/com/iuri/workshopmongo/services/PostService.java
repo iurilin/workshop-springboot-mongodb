1988f39851d4e4958ace850f39657b7cfba69fae
Iuri Lin <audazzk@gmail.com>
2025-04-21 18:25:10 -0300

    Querry with many criteria

--- src/main/java/com/iuri/workshopmongo/services/PostService.java
+++ src/main/java/com/iuri/workshopmongo/services/PostService.java
@@ -1,11 +1,11 @@
 package com.iuri.workshopmongo.services;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.iuri.workshopmongo.domain.Post;
 import com.iuri.workshopmongo.repository.PostRepository;
 import com.iuri.workshopmongo.services.exception.ObjectNotFoundException;
@@ -23,5 +23,10 @@ public class PostService {
 	
 	public List<Post> findByTitle(String text) {
  		return rep.searchTitle(text);
+ 	}
+	
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+ 		maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+ 		return rep.fullSearch(text, minDate, maxDate);
  	}
 }
